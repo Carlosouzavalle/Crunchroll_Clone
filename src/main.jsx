9e8af0b457f0b5f985ca17c64416de74e59d7cfa
Carlosouzavalle <carlosouzavalle@gmail.com>
2024-05-13 00:55:15 -0300

    atualização no carosel

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <Header />
     <Banner />
     <CarroselAnimes />
-    <FreeToWatchCarrousel />
+    <FreeToWatchCarrousel  />
     <MetalRogue />
   </React.StrictMode>,
 )
