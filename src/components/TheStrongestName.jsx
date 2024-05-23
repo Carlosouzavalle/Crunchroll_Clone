e17e2e290e5a5d9e9ff9a1983d4baf1c3fd7cd0e
Carlosouzavalle <carlosouzavalle@gmail.com>
2024-05-22 23:38:20 -0300

    Mudando tudo para ss module

--- src/components/TheStrongestName.jsx
+++ src/components/TheStrongestName.jsx
@@ -1,15 +1,15 @@
 import SpyfamillyNameImg from '../assets/img/87beeb7a51c14bc2aef7571de6d32391.avif'
-import  '../components/css/Thestrongestcss.css'
+import styles from '../components/css/Thestrongestcss.module.css'
 
 
 const TheStrongestName = () => {
     return (
         <>
-            <div className='containerTheStrongest'>
-                <div className='container-img'>
-                    <img src={SpyfamillyNameImg} alt="" />
+            <div className={styles.containerTheStrongest}>
+                <div className={styles.containerImg}>
+                    <img src={SpyfamillyNameImg} alt="" className={styles.strogestImg}/>
                 </div>
-                <div className='contaier-description'>
+                <div className={styles.contaierDescription}>
                     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae suscipit molestiae maiores aspernatur ad explicabo nulla quo, laboriosam dolores voluptatum.</p>
                 </div>
             </div>
