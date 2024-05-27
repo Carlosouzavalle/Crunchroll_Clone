d7f634668b9f8ca45c3c0c3fe2bd1b208ea5a5e9
Carlosouzavalle <carlosouzavalle@gmail.com>
2024-05-27 19:43:25 -0300

    implementaão da drop down

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,33 @@
+import React, { useState } from 'react';
+
+// react icons
 import { CiSearch, CiBookmark, CiUser } from "react-icons/ci";
 import { FaCrown } from "react-icons/fa";
+import { IoMdArrowDropdown } from "react-icons/io";
 
 
 
-import logo from '../assets/logo.svg'
-import crown from '../assets/crown.svg'
-import '../components/css/Header.css'
+import logo from '../assets/logo.svg';
+import crown from '../assets/crown.svg';
+import '../components/css/Header.css';
+import '../components/css/dropdown.css';
 
 const Header = () => {
+
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+    const toggleDropdown = () => {
+        setIsDropdownOpen(!isDropdownOpen);
+    };
+
+
+    const [isDropdownOpen2, setIsDropdownOpen2] = useState(false);
+
+    const toggleDropdown2 = () => {
+        setIsDropdownOpen2(!isDropdownOpen2);
+    };
+
+
     return (
         <nav className="header-components">
             <div className="header-wrapper">
@@ -16,14 +36,56 @@ const Header = () => {
                 </div>
                 <div className="itens-container">
                     <ol className="Itens">
-                        <li><a href="">Browse</a></li>
+                        <li><a href="#" onClick={toggleDropdown2}>Browse <IoMdArrowDropdown className='dropArrow' /></a>
+                            {isDropdownOpen2 && (
+                                <div className='browser-drop'>
+                                    <ul className="dropdown-menu1">
+                                        <li className="dropdown-item">Popular</li>
+                                        <li className="dropdown-item">New</li>
+                                        <li className="dropdown-item">Alphabetical</li>
+                                        <li className="dropdown-item">Simulcast Season</li>
+                                        <li className="dropdown-item">Release Calendar</li>
+                                        <li className="dropdown-item">Music Videos & Concets</li>
+                                    </ul>
+                                    <ul className="dropdown-menu-broser">
+                                        <p className='Genres'>Genres</p>
+                                        <li className="dropdown-item1">Action</li>
+                                        <li className="dropdown-item1">Adventure</li>
+                                        <li className="dropdown-item1">Comedy</li>
+                                        <li className="dropdown-item1">Drama</li>
+                                        <li className="dropdown-item1">Fantasy</li>
+                                        <li className="dropdown-item1">Music</li>
+                                        <li className="dropdown-item1">Romance</li>
+                                        <li className="dropdown-item1">Sci-Fi</li>
+                                        <li className="dropdown-item1">Seinen</li>
+                                        <li className="dropdown-item1">Shojo</li>
+                                        <li className="dropdown-item1">Shonen</li>
+                                        <li className="dropdown-item1">Slice of Life</li>
+                                        <li className="dropdown-item1">Sports</li>
+                                        <li className="dropdown-item1">Supernatural</li>
+                                        <li className="dropdown-item1">Thriller</li>
+                                    </ul>
+                                </div>
+                            )}
+                        </li>
+
+
                         <li><a href="">Games</a></li>
-                        <li><a href="">News</a></li>
+                        <li className="dropdown-container">
+                            <a href="#" onClick={toggleDropdown}>News <IoMdArrowDropdown className='dropArrow' /> </a>
+                            {isDropdownOpen && (
+                                <ul className="dropdown-menu">
+                                    <li className="dropdown-item">All news</li>
+                                    <li className="dropdown-item">Anime Awards</li>
+                                    <li className="dropdown-item">Events & Experiences</li>
+                                </ul>
+                            )}
+                        </li>
                     </ol>
                 </div>
                 <div className="tryfree">
                     <div className="crown">
-                        <FaCrown className="crown-icon"/>
+                        <FaCrown className="crown-icon" />
                     </div>
                     <div className="texts-wrapper">
                         <div className="tryfree-text">
@@ -41,7 +103,7 @@ const Header = () => {
                 </div>
             </div>
         </nav>
-    )
-}
+    );
+};
 
-export default Header
+export default Header;
--- src/components/TheStrongestName.jsx
+++ src/components/TheStrongestName.jsx
@@ -1,16 +1,24 @@
 import SpyfamillyNameImg from '../assets/img/87beeb7a51c14bc2aef7571de6d32391.avif'
 import styles from '../components/css/Thestrongestcss.module.css'
-
+import { CiPlay1, CiBookmark } from "react-icons/ci";
 
 const TheStrongestName = () => {
     return (
         <>
             <div className={styles.containerTheStrongest}>
                 <div className={styles.containerImg}>
-                    <img src={SpyfamillyNameImg} alt="" className={styles.strogestImg}/>
+                    <img src={SpyfamillyNameImg} alt="" className={styles.strogestImg} />
                 </div>
                 <div className={styles.contaierDescription}>
                     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae suscipit molestiae maiores aspernatur ad explicabo nulla quo, laboriosam dolores voluptatum.</p>
+                </div>
+                <div>
+                    <button className={styles.play3}>
+                        <CiPlay1 className={styles.playButton3} />
+                    </button>
+                    <button className={styles.save3}>
+                        <CiBookmark className={styles.saveButton3} />
+                    </button>
                 </div>
             </div>
         </>
